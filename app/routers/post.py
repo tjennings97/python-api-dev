e9449030f0ed1b29f185a5cd86beda97cc4c5006
tjennings97 <tamara.jennings97@gmail.com>
2022-07-18 15:47:29 -0400

    clean up main.py by creating router files for post and user

---
+++ app/routers/post.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from typing import List
+from sqlalchemy.orm.session import Session
+from .. import models, schemas
+from ..database import get_db
+
+router = APIRouter()
+
+@router.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return posts
+
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostBase, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict()) #create new post #** unpacks the fields of the dict object
+    db.add(new_post) #add to database
+    db.commit() #commit new post
+    db.refresh(new_post) #retrieve post and store in new_post
+    return new_post
+
+@router.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", ((str(id)),)) # must pass a tuple for cursor.execute to work
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first() #filter is like doing WHERE in # first grabs first match
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    return post
+
+@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", ((str(id),)))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exist.")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostBase, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exist.")
+    #post_query.update(post.dict(), synchronize_session=False)
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
---
+++ app/routers/user.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm.session import Session
+from .. import models, schemas, utils
+from ..database import get_db
+
+router = APIRouter()
+
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password #update pydantic user model
+
+    new_user = models.User(**user.dict()) #create new post #** unpacks the fields of the dict object
+    db.add(new_user) #add to database
+    db.commit() #commit new post
+    db.refresh(new_user) #retrieve post and store in new_post
+    return new_user
+
+@router.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"User with id {id} does not exist")
+    
+    return user

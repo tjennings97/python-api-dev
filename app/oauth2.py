06aa82ff17beb652c20ef901485e6b450c73c812
tjennings97 <tamara.jennings97@gmail.com>
2022-07-19 15:01:36 -0400

    fix verify_access_token method in oauth2.py, add dependency to create_post in post.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -36,6 +36,8 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
     
+    return token_data
+    
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from typing import List
 from sqlalchemy.orm.session import Session
+
+from app import oauth2
 from .. import models, schemas
 from ..database import get_db
 
@@ -17,7 +19,7 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostBase, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostBase, db: Session = Depends(get_db), get_current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()

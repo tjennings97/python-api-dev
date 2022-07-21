711fea3e4daf84c1853c4e7a3b0e93162ad3fb6c
tjennings97 <tamara.jennings97@gmail.com>
2022-07-21 16:55:25 -0400

    organize libraries, add limit/skip/search query parameter handling, update routes to require certain users to be logged in to perform actions

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-from typing import List
-from sqlalchemy.orm.session import Session
+from typing import List, Optional
 
-from app import oauth2
-from .. import models, schemas
+from fastapi import (APIRouter, Depends, FastAPI, HTTPException, Response,
+                     status)
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.functions import mode
+
+from .. import models, oauth2, schemas
 from ..database import get_db
 
 router = APIRouter(
@@ -12,10 +14,12 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all() # get all posts for logged in user
+
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all() # get all posts for all users
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -25,8 +29,7 @@ def create_posts(post: schemas.PostBase, db: Session = Depends(get_db), current_
     # conn.commit()
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
 
-    print(current_user.email)
-    new_post = models.Post(**post.dict()) #create new post #** unpacks the fields of the dict object
+    new_post = models.Post(owner_id=current_user.id, **post.dict()) #create new post #** unpacks the fields of the dict object
     db.add(new_post) #add to database
     db.commit() #commit new post
     db.refresh(new_post) #retrieve post and store in new_post
@@ -37,9 +40,17 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db), current
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", ((str(id)),)) # must pass a tuple for cursor.execute to work
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first() #filter is like doing WHERE in # first grabs first match
-    if post == None:
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
+
+    # if you want posts to only be accessed by their creator 
+    ''' 
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+                            detail=f"Not authorized to perform requested action")
+    '''
+
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -47,11 +58,18 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", ((str(id),)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} does not exist.")
-    post.delete(synchronize_session=False)
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -66,6 +84,10 @@ def update_post(id: int, updated_post: schemas.PostBase, db: Session = Depends(g
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} does not exist.")
     #post_query.update(post.dict(), synchronize_session=False)
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Not authorized to perform requested action")
+
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()

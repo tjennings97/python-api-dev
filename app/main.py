689d28290a2bd959cd34e5529741585aad4464e1
tjennings97 <tamara.jennings97@gmail.com>
2022-07-21 16:48:49 -0400

    clean up: removing unused libraries

--- app/main.py
+++ app/main.py
@@ -1,33 +1,12 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
+from fastapi import FastAPI
 
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from sqlalchemy.sql.functions import mode
 from . import models
 from .database import engine
-from .routers import post, user, auth
+from .routers import auth, post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', 
-                                password='password', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
 
 app.include_router(post.router) #include our post.router
 app.include_router(user.router) #include router object from user file, imports specific routes

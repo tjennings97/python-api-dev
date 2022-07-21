6ea45985b7045181d9505c47152f2fa887833b58
tjennings97 <tamara.jennings97@gmail.com>
2022-07-21 16:52:25 -0400

    add foreign key and owner-User relationship to Post table schema

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from sqlalchemy import Column
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import null, text
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP, Boolean, Integer, String
+
 from .database import Base
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -11,6 +15,10 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User") # fetches user for us
 
 class User(Base):
     __tablename__="users"

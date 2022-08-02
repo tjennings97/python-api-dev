038c716cf84511b0ee9798d9da779646ce2f46e2
tjennings97 <tamara.jennings97@gmail.com>
2022-08-02 08:57:06 -0400

    update database & oauth2 to use env variables

--- app/database.py
+++ app/database.py
@@ -5,8 +5,9 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,12 @@
-from fastapi import Depends, status, HTTPException
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
 
-from app import models, schemas, database
+from app import database, models, schemas
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login') #grab route in this case '/login'
 
@@ -12,9 +14,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login') #grab route in this case 
 #Alggorithm
 #Expiration time
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" #random string
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key #random string
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
